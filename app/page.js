2a49791854d53916fce814aafcf42ddef25c89ed
Will Webb <willwwebb1@gmail.com>
2026-02-22 14:04:54 -0500

    push

--- app/page.js
+++ app/page.js
@@ -1,10 +1,3 @@
-import Home from '@/components/Home/Home'
-
-export const metadata = {
-  title: 'Reformed Chapter - Bible Study Resources',
-  description: 'Comprehensive Bible study resources organized by chapter',
-}
-
 export default function HomePage() {
-  return <Home />
+  return <h1>Hello World</h1>
 }
